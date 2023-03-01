de168efcc6e38d2bb119df9e90387aa666d9507d
Laimusp <armis3005@gmail.com>
2023-03-01 16:05:02 +0300

    Латте макиато

--- release/main.py
+++ release/main.py
@@ -1,20 +1,139 @@
 import sys
 import sqlite3
 
-from PyQt5 import uic
 from PyQt5.QtWidgets import QMainWindow, QApplication, QTableWidgetItem
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class MyWidget(QMainWindow):
+class MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(802, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.tableWidget = QtWidgets.QTableWidget(self.centralwidget)
+        self.tableWidget.setGeometry(QtCore.QRect(0, 140, 801, 461))
+        self.tableWidget.setObjectName("tableWidget")
+        self.tableWidget.setColumnCount(0)
+        self.tableWidget.setRowCount(0)
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(60, 60, 181, 51))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(550, 60, 181, 51))
+        self.pushButton_2.setObjectName("pushButton_2")
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Показать"))
+        self.pushButton_2.setText(_translate("MainWindow", "Добавить"))
+
+
+class AddForm(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(608, 596)
+        self.pushButton = QtWidgets.QPushButton(Form)
+        self.pushButton.setGeometry(QtCore.QRect(500, 190, 91, 31))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(Form)
+        self.pushButton_2.setGeometry(QtCore.QRect(500, 550, 91, 31))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.label_8 = QtWidgets.QLabel(Form)
+        self.label_8.setGeometry(QtCore.QRect(26, 260, 51, 20))
+        self.label_8.setObjectName("label_8")
+        self.spinBox = QtWidgets.QSpinBox(Form)
+        self.spinBox.setGeometry(QtCore.QRect(80, 260, 511, 22))
+        self.spinBox.setObjectName("spinBox")
+        self.pushButton_3 = QtWidgets.QPushButton(Form)
+        self.pushButton_3.setGeometry(QtCore.QRect(20, 290, 571, 23))
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.pushButton_4 = QtWidgets.QPushButton(Form)
+        self.pushButton_4.setGeometry(QtCore.QRect(20, 320, 571, 23))
+        self.pushButton_4.setObjectName("pushButton_4")
+        self.tableWidget = QtWidgets.QTableWidget(Form)
+        self.tableWidget.setGeometry(QtCore.QRect(20, 350, 571, 192))
+        self.tableWidget.setObjectName("tableWidget")
+        self.tableWidget.setColumnCount(0)
+        self.tableWidget.setRowCount(0)
+        self.lineEdit = QtWidgets.QLineEdit(Form)
+        self.lineEdit.setGeometry(QtCore.QRect(150, 20, 331, 20))
+        self.lineEdit.setObjectName("lineEdit")
+        self.label = QtWidgets.QLabel(Form)
+        self.label.setGeometry(QtCore.QRect(30, 20, 91, 16))
+        self.label.setObjectName("label")
+        self.lineEdit_2 = QtWidgets.QLineEdit(Form)
+        self.lineEdit_2.setGeometry(QtCore.QRect(150, 50, 331, 20))
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.label_2 = QtWidgets.QLabel(Form)
+        self.label_2.setGeometry(QtCore.QRect(30, 50, 101, 16))
+        self.label_2.setObjectName("label_2")
+        self.lineEdit_3 = QtWidgets.QLineEdit(Form)
+        self.lineEdit_3.setGeometry(QtCore.QRect(150, 80, 331, 20))
+        self.lineEdit_3.setObjectName("lineEdit_3")
+        self.label_3 = QtWidgets.QLabel(Form)
+        self.label_3.setGeometry(QtCore.QRect(30, 80, 111, 16))
+        self.label_3.setObjectName("label_3")
+        self.lineEdit_4 = QtWidgets.QLineEdit(Form)
+        self.lineEdit_4.setGeometry(QtCore.QRect(150, 110, 331, 20))
+        self.lineEdit_4.setObjectName("lineEdit_4")
+        self.label_4 = QtWidgets.QLabel(Form)
+        self.label_4.setGeometry(QtCore.QRect(30, 110, 111, 16))
+        self.label_4.setObjectName("label_4")
+        self.lineEdit_5 = QtWidgets.QLineEdit(Form)
+        self.lineEdit_5.setGeometry(QtCore.QRect(150, 140, 331, 20))
+        self.lineEdit_5.setObjectName("lineEdit_5")
+        self.label_5 = QtWidgets.QLabel(Form)
+        self.label_5.setGeometry(QtCore.QRect(30, 140, 111, 16))
+        self.label_5.setObjectName("label_5")
+        self.lineEdit_6 = QtWidgets.QLineEdit(Form)
+        self.lineEdit_6.setGeometry(QtCore.QRect(150, 170, 331, 20))
+        self.lineEdit_6.setObjectName("lineEdit_6")
+        self.label_6 = QtWidgets.QLabel(Form)
+        self.label_6.setGeometry(QtCore.QRect(30, 170, 41, 16))
+        self.label_6.setObjectName("label_6")
+        self.lineEdit_7 = QtWidgets.QLineEdit(Form)
+        self.lineEdit_7.setGeometry(QtCore.QRect(150, 200, 331, 20))
+        self.lineEdit_7.setObjectName("lineEdit_7")
+        self.label_7 = QtWidgets.QLabel(Form)
+        self.label_7.setGeometry(QtCore.QRect(30, 200, 111, 16))
+        self.label_7.setObjectName("label_7")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.pushButton.setText(_translate("Form", "Добавить"))
+        self.pushButton_2.setText(_translate("Form", "Отмена"))
+        self.label_8.setText(_translate("Form", "ID:"))
+        self.pushButton_3.setText(_translate("Form", "Загрузить"))
+        self.pushButton_4.setText(_translate("Form", "Сохранить"))
+        self.label.setText(_translate("Form", "id"))
+        self.label_2.setText(_translate("Form", "Название сорта"))
+        self.label_3.setText(_translate("Form", "Степень обжарки"))
+        self.label_4.setText(_translate("Form", " Молотый/в зернах"))
+        self.label_5.setText(_translate("Form", "Описание вкуса"))
+        self.label_6.setText(_translate("Form", "Цена"))
+        self.label_7.setText(_translate("Form", "Объем упаковки"))
+
+
+class MyWidget(QMainWindow, MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('release/UI/main.ui', self)
+        self.setupUi(self)
         self.pushButton.clicked.connect(self.loadTable)
         self.pushButton_2.clicked.connect(self.add)
         self.tableWidget.setColumnCount(7)
         self.tableWidget.setHorizontalHeaderLabels(
             ['ID', 'название сорта', 'степень обжарки', 'молотый/в зернах', 'описание вкуса', 'цена', 'объем упаковки'])
-        self.con = sqlite3.connect('release/data/coffee.sqlite')
+        self.con = sqlite3.connect('data/coffee.sqlite')
 
     def loadTable(self):
         cur = self.con.cursor()
@@ -26,18 +145,18 @@ class MyWidget(QMainWindow):
                 self.tableWidget.setItem(i, j, QTableWidgetItem(str(elem)))
 
     def add(self):
-        self.second_form = AddForm()
+        self.second_form = AddFormWindow()
         self.second_form.show()
         self.close()
 
 
-class AddForm(QMainWindow):
+class AddFormWindow(QMainWindow, AddForm):
     def __init__(self):
         super().__init__()
-        uic.loadUi('release/UI/addEditCoffeeForm.ui', self)
+        super().setupUi(self)
         self.pushButton.clicked.connect(self.add)
         self.pushButton_2.clicked.connect(self.cancel)
-        self.con = sqlite3.connect('release/data/coffee.sqlite')
+        self.con = sqlite3.connect('data/coffee.sqlite')
         self.pushButton_3.clicked.connect(self.update_result)
         self.tableWidget.itemChanged.connect(self.item_changed)
         self.pushButton_4.clicked.connect(self.save_results)
@@ -95,7 +214,12 @@ class AddForm(QMainWindow):
         self.close()
 
 
+def excepthook(exc_type, exc_value, traceback):
+    return ''.join(traceback.format_exception(exc_type, exc_value, traceback))
+
+
 if __name__ == '__main__':
+    sys.excepthook = excepthook
     app = QApplication(sys.argv)
     ex = MyWidget()
     ex.show()
